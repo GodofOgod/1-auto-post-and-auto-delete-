ee35d4eaedfae15f6c90183b5e01083e4942c847
GodofOgod <157294914+GodofOgod@users.noreply.github.com>
2025-08-22 12:22:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ DB_NAME = os.environ.get("DB_NAME", "Channelbroadcastbot")
 
 # Optional: List of default Telegram channels)
 # You can add unlimited channel directy from bot)
-DEFAULT_CHANNELS = list(map(int, os.environ.get("DEFAULT_CHANNELS", "-1003018068777").split(",")))      #-1001234567890,-1009876543210  For Example
+DEFAULT_CHANNELS = list(map(int, os.environ.get("DEFAULT_CHANNELS", "-1002708988775,-1002524893260,-1002293619404,-1002447252037,-1002617751817,-1002599212023,-1002783719315").split(",")))      #-1001234567890,-1009876543210  For Example
 
 DELETE_TIME = int(environ.get("DELETE_TIME", "30"))  #  deletion time in seconds (default: 5 minutes). Adjust as per your needs.
