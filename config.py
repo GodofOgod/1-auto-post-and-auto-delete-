539e9768eb67efd5edd192f8556ebe7271a1b7ad
GodofOgod <157294914+GodofOgod@users.noreply.github.com>
2025-08-22 07:00:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 from os import environ
 
 # Telegram Bot Token from @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8356551419:AAGFZnpBbEaYeUh-t2CP-XGU3gMazpHX_pw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8095429243:AAGJjnNYZgcb6EAGduIXc7D4V0WvRqxJeVo")
 
 # List of authorized user IDs (Admins)
-AUTHORIZED_USERS = list(map(int, os.environ.get("AUTHORIZED_USERS", "1397269319,6906650389").split(",")))      #For Example 123456,123456
+AUTHORIZED_USERS = list(map(int, os.environ.get("AUTHORIZED_USERS", "1397269319,6906650389,1489652480").split(",")))      #For Example 123456,123456
 
 # MongoDB URL and DB name
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://wemedia360:Ex3pPKO0PNBWcI6k@channelbroadcastbot.pqhuvuc.mongodb.net/?retryWrites=true&w=majority&appName=Channelbroadcastbot")
@@ -13,6 +13,6 @@ DB_NAME = os.environ.get("DB_NAME", "Channelbroadcastbot")
 
 # Optional: List of default Telegram channels)
 # You can add unlimited channel directy from bot)
-DEFAULT_CHANNELS = list(map(int, os.environ.get("DEFAULT_CHANNELS", "-1002805570628,-1001842318978").split(",")))      #-1001234567890,-1009876543210  For Example
+DEFAULT_CHANNELS = list(map(int, os.environ.get("DEFAULT_CHANNELS", "-1002399067702,-1001842318978").split(",")))      #-1001234567890,-1009876543210  For Example
 
-DELETE_TIME = int(environ.get("DELETE_TIME", "300"))  #  deletion time in seconds (default: 5 minutes). Adjust as per your needs.
+DELETE_TIME = int(environ.get("DELETE_TIME", "3600"))  #  deletion time in seconds (default: 5 minutes). Adjust as per your needs.
